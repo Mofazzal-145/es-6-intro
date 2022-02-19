9885324f208f731783213398d29c44a03e260f37
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-20 00:46:16 +0600

    basic file

---
+++ basic.js
@@ -0,0 +1,62 @@
+
+// variable
+var deposit = 400;
+
+// condition
+if(deposit > 500){
+
+}
+else if(deposit < 200){
+
+}
+else if(deposit == 500){
+
+}
+else if(deposit != 500){
+
+}
+else if(deposit >= 500){
+
+}
+else if(deposit <= 500){
+
+}
+else{
+
+}
+
+// array
+
+const numbers = [12, 34, 45, 56, 76, 78, 123];
+const numberCount = numbers.length;
+numbers.pop();
+numbers.push(111);
+numbers[2] = 55;
+// check weather 222 included in the array
+if(numbers.indexOf(222) != -1){
+
+}
+if(numbers.includes(222)){
+
+}
+
+// loop
+// while, for
+
+for (const number of numbers){
+
+}
+
+// function
+
+function fullName (first, second){
+    const name = first + ' ' + second;
+}
+
+const person = fullName('Rahim', 'Alil');
+
+//object
+
+const bottle = {color: 'yellow',contain: 'water', price: '200'} ;
+
+
